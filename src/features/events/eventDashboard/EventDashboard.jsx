34b91d33083da7a765e764abcf6615bda5d21068
linhpcm <pcmlinh1112@gmail.com>
2020-08-09 15:06:52 +0700

    CRUD Operation

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,16 +4,39 @@ import EventList from './EventList';
 import EventForm from '../evetnForm/EventForm';
 import {sampleData} from '../../../app/api/sampleData';
 
-const EventDashboard = ({formOpen, setFormOpen}) => {
+const EventDashboard = ({formOpen, setFormOpen, selectEvent, selectedEvent}) => {
     const [events, setEvents] = useState(sampleData);
+
+    function handleCreateEvent(event) {
+        setEvents([...events, event])
+    }
+
+    function handleUpdateEvent(updatedEvent) {
+        setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+        selectEvent(null);
+        setFormOpen(false); 
+    }
+
+    function handleDeleteEvent(eventId) {
+        setEvents(events.filter(evt => evt.id !== eventId))
+    }
 
     return (
         <Grid>
             <Grid.Column width={10}>
-                <EventList events={sampleData} />
+                <EventList events={events} 
+                    selectEvent={selectEvent}
+                    deleteEvent={handleDeleteEvent} />
             </Grid.Column>
             <Grid.Column width={6}>
-                {formOpen && <EventForm setFormOpen={setFormOpen} />}
+                {formOpen && 
+                <EventForm 
+                    setFormOpen={setFormOpen} 
+                    setEvents={setEvents}
+                    createEvent={handleCreateEvent}
+                    selectedEvent={selectedEvent}
+                    updateEvent={handleUpdateEvent}
+                    key={selectedEvent ? selectedEvent.id : null} />}
             </Grid.Column>
         </Grid>
     )
